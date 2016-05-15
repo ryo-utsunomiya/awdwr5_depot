ab7e07a4df3b7c1bfb5268f6528d2a6e0ca71f60
Ryo Utsunomiya <millionsnowliving@gmail.com>
2016-05-15 18:06:27 +0900

    Fix typo

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -51,12 +51,12 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test 'product is not valid without a unique title' do
-    product = Product.new(titile:      products(:ruby).title,
+    product = Product.new(title:       products(:ruby).title,
                           description: 'yyy',
                           price:       1,
                           image_url:   'fred.gif')
     assert product.invalid?
-    assert_equal ['has already been taken'], product.errors[:titile]
+    assert_equal ['has already been taken'], product.errors[:title]
   end
 
   test 'product is not valid without a unique title - i18n' do
